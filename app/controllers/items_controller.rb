d4b741b6ed5f5ae163432e0ea0500152638ba03e
ReleLes <60468150+ReleLes@users.noreply.github.com>
2020-10-23 14:21:21 +0900

    Rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
-  #ログインしていないユーザーはindex意外には遷移できず、ログイン画面に遷移する
+  # ログインしていないユーザーはindex意外には遷移できず、ログイン画面に遷移する
   before_action :move_to_sign, except: [:index]
 
   def index
     @items = Item.all
   end
-  
+
   def new
     @item = Item.new
   end
@@ -20,13 +20,12 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :title, :explain, :category_id, :status_id, :charge_id, :area_id, :arrival_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_sign
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,18 @@
 class Item < ApplicationRecord
-  #↓があることで belongs_to_active_hashメソッドが使用できる
+  # ↓があることで belongs_to_active_hashメソッドが使用できる
   extend ActiveHash::Associations::ActiveRecordExtensions
-  #activehashを使用したアソシエーション
+  # activehashを使用したアソシエーション
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :charge
   belongs_to_active_hash :area
   belongs_to_active_hash :arrival
 
-  #アソシエーション
+  # アソシエーション
   belongs_to :user
   has_one_attached :image
 
-  #空の情報は保存できない
+  # 空の情報は保存できない
   validates :title, presence: true
   validates :explain, presence: true
   validates :category, presence: true
@@ -23,17 +23,17 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :image, presence: true
 
-  #選択時が「--」の時は保存できないようにする
+  # 選択時が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :charge_id, numericality: { other_than: 1 }
   validates :area_id, numericality: { other_than: 1 }
   validates :arrival_id, numericality: { other_than: 1 }
 
-  #priceには半角数字のみ保存できる
-  VALID_PRICE_REGEX = /\A[0-9]+\z/i
+  # priceには半角数字のみ保存できる
+  VALID_PRICE_REGEX = /\A[0-9]+\z/i.freeze
   validates :price, format: { with: VALID_PRICE_REGEX }
 
-  #価格の範囲のバリデーション(300〜9999999)
-  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
+  # 価格の範囲のバリデーション(300〜9999999)
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
