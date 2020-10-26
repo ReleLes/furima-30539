35c16570158ee0aef8fe8ddc2abc35e334f6b208
ReleLes <60468150+ReleLes@users.noreply.github.com>
2020-10-26 13:00:45 +0900

    ログアウトしているユーザーも詳細に遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはindex意外には遷移できず、ログイン画面に遷移する
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:show]
 
   def index
     @items = Item.all.order("created_at DESC")
